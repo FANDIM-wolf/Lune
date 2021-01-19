6462d298b06fca1905df3d2b0e4b077654e04eca
Mikhail Shtainer <63235817+FANDIM-wolf@users.noreply.github.com>
2021-01-19 22:21:28 +0300

    Add files via upload

---
+++ Lune/scripts/script.js
@@ -0,0 +1,9 @@
+
+const testLike = document.getElementById("test-like");
+
+//to add event function 
+
+document.addEventListener('DOMContentLoaded',function(){
+    alert("Dom document loaded");
+    
+});
